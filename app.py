a8335118939dd385bdf425c8249777266769c34e
Athanasios Chantis <athanasiosc@yahoo.com>
2021-03-21 09:12:21 -0400

    Changes in app.py

--- app.py
+++ app.py
@@ -5,24 +5,13 @@ import datetime as dt
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 
-st.title("S&P500 Stocks")
-# Get S&P500 stock tickers
-stocks = pd.read_csv('/Users/chantis/Desktop/ipynb_checkpoints/SP500.csv')
-stocks.head()
-symbols = stocks['Symbol'].sort_values().tolist()
+st.title("AC TDI 12-day Program Milestone")
 
+# Get the stock ticker from the user 
 ticker = st.sidebar.text_input("Enter ticker (e.g., AAPL)")
-
-# Create a drop down menu for stock stickers
-# This also will define the value of ticker in subsequent code
-#ticker = st.sidebar.selectbox('Choose a S&P 500 Stock', symbols)  
-
-#Connect ticker to yfinance 
-stock = yf.Ticker(ticker)
-st.title(ticker)
          
 # Create two radio buttons 1. "Fundamental" 2. "Technical" analysis
-# The buttons are given as Tuple
+
 infoType = st.sidebar.radio("Choose an info type",('Fundamental', 'Technical')) 
 
 if(infoType == 'Fundamental'):
@@ -31,6 +20,8 @@ else:
     start = dt.datetime.today()-dt.timedelta(2 * 365)
     end = dt.datetime.today()
     df = yf.download(ticker,start,end)
+    if df.empty:
+        st.write(ticker+": No data found, symbol may be delisted")
     df = df.reset_index()
          
     fig = go.Figure(
